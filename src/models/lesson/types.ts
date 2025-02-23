23265f9ded524231c42ac5518deb581ec80190db
Kenneth <dao.khoi@1bytesoftware.com>
2025-02-23 13:56:33 +0700

    fix: remove deprecated in IGetLessonFilterParams

--- src/models/lesson/types.ts
+++ src/models/lesson/types.ts
@@ -6,88 +6,88 @@ import { IQuestionType } from '../questionType';
 import { LESSON_STATUS } from './constants';
 
 export interface ILesson extends IBaseObject {
-  soundBeep: false;
+    soundBeep: false;
 
-  userIdAsAppover?: JfwIdType;
-  languageId: number;
+    userIdAsAppover?: JfwIdType;
+    languageId: number;
 
-  /**
-   * @deprecated Use questionTypes instead
-   */
-  categories: ICategory[];
-  questionTypes: IQuestionType[];
-  medias: IMedia[];
-  priorities: IPriority[];
-  questionGroups: IQuestionGroup[];
-  lessonsSeeAlso: ILesson[];
-  translations: ILesson[];
-  source?: ISource;
+    /**
+     * @deprecated Use questionTypes instead
+     */
+    categories: ICategory[];
+    questionTypes: IQuestionType[];
+    medias: IMedia[];
+    priorities: IPriority[];
+    questionGroups: IQuestionGroup[];
+    lessonsSeeAlso: ILesson[];
+    translations: ILesson[];
+    source?: ISource;
 
-  title: string;
-  content: string;
-  duration: number;
-  explanation: string;
-  isFree: boolean;
-  transcript: string;
-  transcriptTranslated?: string;
-  description: string | null;
-  preparationTime: number;
+    title: string;
+    content: string;
+    duration: number;
+    explanation: string;
+    isFree: boolean;
+    transcript: string;
+    transcriptTranslated?: string;
+    description: string | null;
+    preparationTime: number;
 
-  lessonOriginalSourceLink?: string;
-  lessonOriginalSourceMediaLink?: string;
-  practiced: boolean;
-  shared: boolean;
-  allowSkipAnswer: boolean;
-  allowSkipPreparationTime: boolean;
-  allowSkipDuration: boolean;
-  isSystem: boolean;
-  privateNotes: string;
+    lessonOriginalSourceLink?: string;
+    lessonOriginalSourceMediaLink?: string;
+    practiced: boolean;
+    shared: boolean;
+    allowSkipAnswer: boolean;
+    allowSkipPreparationTime: boolean;
+    allowSkipDuration: boolean;
+    isSystem: boolean;
+    privateNotes: string;
 
-  zorder: number;
-  status: LESSON_STATUS;
-  statusValue: string;
+    zorder: number;
+    status: LESSON_STATUS;
+    statusValue: string;
 }
 
 export interface IPriority extends IBaseObject {
-  categoryId: IdType;
-  lessonId: IdType;
+    categoryId: IdType;
+    lessonId: IdType;
 }
 
 export interface ISource {
-  id: IdType;
-  sourceName?: string;
-  sourceWebsite?: string;
-  description?: string;
+    id: IdType;
+    sourceName?: string;
+    sourceWebsite?: string;
+    description?: string;
 }
 
 export interface ILessonForm extends Partial<IBaseObject> {
-  questionTypeId?: IdType;
-  questionTypes: IQuestionType[];
-  questionGroups: IQuestionGroup[];
-  medias: IMedia[];
+    questionTypeId?: IdType;
+    questionTypes: IQuestionType[];
+    questionGroups: IQuestionGroup[];
+    medias: IMedia[];
 
-  title?: string;
-  content?: string;
-  duration: number;
-  description?: string;
-  explanation?: string;
-  isFree: boolean;
-  preparationTime: number;
-  transcript?: string;
-  allowSkipAnswer: boolean;
-  allowSkipPreparationTime: boolean;
-  allowSkipDuration: boolean;
-  soundBeep: boolean;
+    title?: string;
+    content?: string;
+    duration: number;
+    description?: string;
+    explanation?: string;
+    isFree: boolean;
+    preparationTime: number;
+    transcript?: string;
+    allowSkipAnswer: boolean;
+    allowSkipPreparationTime: boolean;
+    allowSkipDuration: boolean;
+    soundBeep: boolean;
 
-  isSystem: boolean;
-  privateNotes?: string;
-  status: LESSON_STATUS;
-  zOrder?: number | null;
+    isSystem: boolean;
+    privateNotes?: string;
+    status: LESSON_STATUS;
+    zOrder?: number | null;
 }
 
 export interface ILessonVocab extends IBaseObject {
-  lessonId: IdType;
-  vocabId: IdType;
+    lessonId: IdType;
+    vocabId: IdType;
 }
 
 /**
@@ -97,62 +97,58 @@ export interface ILessonVocab extends IBaseObject {
 export type IGetListLessonsParams = IGetLessonFilterParams;
 
 export interface IGetLessonFilterParams extends IPageable, ISortable {
-  /**
-   * @deprecated
-   * Use IGetLessonsFilterParams instead
-   */
-  categoryIds?: IdType;
-  questionTypeId?: IdType;
+    categoryIds?: string;
+    questionTypeId?: IdType;
 
-  languageCode?: string;
+    languageCode?: string;
 
-  title?: string;
-  content?: string;
-  keyword?: string;
-  status?: string;
-  isFree?: boolean;
-  isPracticed?: boolean;
-  shared?: boolean;
-  timeType?: string;
-  zOrder?: number;
+    title?: string;
+    content?: string;
+    keyword?: string;
+    status?: string;
+    isFree?: boolean;
+    isPracticed?: boolean;
+    shared?: boolean;
+    timeType?: string;
+    zOrder?: number;
 }
 
 export interface IQueryLessonParams {
-  /**
-   * @deprecated Use questionTypeCode instead
-   */
-  categoryCode?: string;
+    /**
+     * @deprecated Use questionTypeCode instead
+     */
+    categoryCode?: string;
 
-  questionTypeCode: string;
-  zOrder: number;
+    questionTypeCode: string;
+    zOrder: number;
 }
 
 export interface IGetLessonExportParams {
-  categoryCode: string;
+    categoryCode: string;
 }
 export interface IAddSeeAlsoPayload {
-  content: string;
-  countryId: JfwIdType;
-  categories: [
-    {
-      id: IdType;
-    },
-  ];
-  languageCode?: string;
-  description?: string;
-  medias?: IMedia[];
+    content: string;
+    countryId: JfwIdType;
+    categories: [
+        {
+            id: IdType;
+        },
+    ];
+    languageCode?: string;
+    description?: string;
+    medias?: IMedia[];
 }
 
 export interface ICreateLessonVocabParams {
-  id: IdType;
-  vocabIds: IdType[];
+    id: IdType;
+    vocabIds: IdType[];
 }
 export interface IDeleteLessonVocabParams extends ICreateLessonVocabParams {}
 
 export interface ICreateLessonPriorityParams {
-  lessonId: IdType;
-  categoryId: IdType;
-  userId: JfwIdType;
+    lessonId: IdType;
+    categoryId: IdType;
+    userId: JfwIdType;
 }
 
 export type IDeleteLessonPriorityParams = ICreateLessonPriorityParams;
